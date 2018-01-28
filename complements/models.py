7098f65fb51b465438d280c86063bbf6a6e11499
Humboldt155 <petr_kozyrev@icloud.com>
2018-01-28 22:54:28 +0300

    10

--- complements/models.py
+++ complements/models.py
@@ -6,6 +6,11 @@ class Department(models.Model):
     department_id = models.CharField(max_length=2, primary_key=True, unique=True)
     department_name = models.CharField(max_length=40)
 
+    class Meta:
+        ordering = ["department_id"]
+        verbose_name = "отдел"
+        verbose_name_plural = "отделы"
+
     def __str__(self):
         return self.department_id
 
@@ -16,6 +21,11 @@ class Model(models.Model):
     model_fr_name = models.CharField(max_length=200)
     model_en_name = models.CharField(max_length=200)
     model_ru_name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["model_id"]
+        verbose_name = "модель Адео"
+        verbose_name_plural = "модели Адео"
 
     def __str__(self):
         return self.model_id
@@ -29,6 +39,11 @@ class Attribute(models.Model):
     attribute_en_name = models.CharField(max_length=200)
     attribute_ru_name = models.CharField(max_length=200)
 
+    class Meta:
+        ordering = ["attribute_id"]
+        verbose_name = "атрибут"
+        verbose_name_plural = "атрибуты"
+
     def __str__(self):
         return self.attribute_id
 
@@ -39,6 +54,11 @@ class Value(models.Model):
     value_fr_name = models.CharField(max_length=200)
     value_en_name = models.CharField(max_length=200)
     value_ru_name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["value_id"]
+        verbose_name = "значение атрибута"
+        verbose_name_plural = "значения атрибутов"
 
     def __str__(self):
         return self.value_id
@@ -51,6 +71,11 @@ class LMCode(models.Model):
     lmcode_model = models.ForeignKey(Model)
     lmcode_department = models.ForeignKey(Department)
 
+    class Meta:
+        ordering = ["lmcode_id"]
+        verbose_name = "lm код"
+        verbose_name_plural = "lm коды"
+
     def __str__(self):
         return self.lmcode_id
 
@@ -58,10 +83,15 @@ class LMCode(models.Model):
 class Link(models.Model):
     link_model = models.ForeignKey(Model)
     link_attribute = models.ForeignKey(Attribute)
-    link_value = models.ForeignKey(Value)
+    link_value = models.ForeignKey(Value, blank=True)
     link_name = str(link_model) + " + " + str(link_attribute) + " + " + str(link_value)
+
+    class Meta:
+        ordering = ["link_value", "link_attribute", "link_model"]
+        unique_together = ("link_model", "link_attribute", "link_value")
+        verbose_name = "связь"
+        verbose_name_plural = "связи"
 
     def __str__(self):
         return self.link_name
-
 
