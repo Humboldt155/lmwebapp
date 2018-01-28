0b36e17a3a2798f4c2354baa8b615221d258afbf
Humboldt155 <petr_kozyrev@icloud.com>
2018-01-28 15:58:12 +0300

    ok4

--- complements/admin.py
+++ complements/admin.py
@@ -4,9 +4,13 @@ from .models import Department
 from .models import Model
 from .models import Attribute
 from .models import Value
+from .models import LMCode
+from .models import Link
 
 admin.site.register(Department)
 admin.site.register(Model)
 admin.site.register(Attribute)
 admin.site.register(Value)
+admin.site.register(LMCode)
+admin.site.register(Link)
 
--- complements/models.py
+++ complements/models.py
@@ -10,7 +10,7 @@ class Department(models.Model):
         return self.department_id
 
 
-
+# Model Adeo
 class Model(models.Model):
     model_id = models.CharField(max_length=10, primary_key=True, unique=True)
     model_fr_name = models.CharField(max_length=200)
@@ -21,9 +21,10 @@ class Model(models.Model):
         return self.model_id
 
 
-
+# Attribute
 class Attribute(models.Model):
     attribute_id = models.CharField(max_length=9, primary_key=True, unique=True)
+    attribute_limited = models.BooleanField()
     attribute_fr_name = models.CharField(max_length=200)
     attribute_en_name = models.CharField(max_length=200)
     attribute_ru_name = models.CharField(max_length=200)
@@ -32,7 +33,7 @@ class Attribute(models.Model):
         return self.attribute_id
 
 
-
+# Value
 class Value(models.Model):
     value_id = models.CharField(max_length=10, primary_key=True, unique=True)
     value_fr_name = models.CharField(max_length=200)
@@ -41,3 +42,37 @@ class Value(models.Model):
 
     def __str__(self):
         return self.value_id
+
+#LMCode
+class LMCode(models.Model):
+    lmcode_id = models.CharField(max_length=8, primary_key=True, unique=True)
+    lmcode_name = models.CharField(max_length=200)
+    lmcode_avs = models.DateField()
+    lmcode_model = models.ForeignKey(Model)
+    lmcode_department = models.ForeignKey(Department)
+
+    def __str__(self):
+        return self.lmcode_id
+
+#Link - принадлежность значений и атрибутов к моделям. Одной записи соответствует одно значение
+class Link(models.Model):
+    link_model = models.ForeignKey(Model)
+    link_attribute = models.ForeignKey(Attribute)
+    link_value = models.ForeignKey(Value)
+    link_name = str(link_model) + " + " + str(link_attribute) + " + " + str(link_value)
+
+    def __str__(self):
+        return self.link_name
+
+"""
+#LMCodeValue
+class LMCodeValue(models.Model):
+    lmcode_id = models.ForeignKey(LMCode)
+    attribute_id = models.ForeignKey(Attribute)
+    value_id = models.ForeignKey(Value, default=None)
+    value_not_limited = models.DecimalField(default=None)
+    name = str(lmcode_id) + " - " + str(attribute_id)
+
+    def __str__(self):
+        return self.name
+"""
